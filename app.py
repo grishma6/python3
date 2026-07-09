fa65190d1819a2f9c262c8b4b6c1f7833f786095
grishma6 <gxg59150@ucmo.edu>
2026-07-09 18:07:46 -0500

    operators

---
+++ app.py
@@ -0,0 +1,32 @@
+# Type conversion
+# int(x)
+# float(x)
+# str(x)
+# bool(x)
+# x = input("x: ")
+# y = int(x) + 1
+# print(f"x: {x}, y:{y}")
+
+# Falsy
+# ""
+# 0
+# None
+
+# ***************************************
+
+# Conditional Statements
+temperature = 5
+if temperature > 30:
+    print("Grishma")
+    print("janu")
+elif temperature < 30:
+    print("less Temperature")
+else:
+    print("Good")
+print("Done")
+
+# *****************************
+
+age = 20
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
